ea36fffde1a042f4352df2a3fcaa5f66e01e3c66
managai <bruno.santeramo@gmail.com>
2017-03-18 21:57:32 +0100

    __init__.py

---
+++ rubrica/__init__.py
@@ -0,0 +1,7 @@
+'A Django address book application by manax team.'
+VERSION = (0, 0, 0)
+__version__ = ".".join(map(str, VERSION))
+__url__ = 'https://github.com/manax-dojo/manax-rubrica'
+__author__ = 'Bruno Santeramo'
+__email__ = ''
+__license__ = 'BSD License'

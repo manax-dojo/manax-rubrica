b9957741e7838be2542c5b6f1339805ec6082045
managai <bruno.santeramo@gmail.com>
2017-03-18 21:53:49 +0100

    App skeleton

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # manax-rubrica
+
+Inspired by
+* https://github.com/myles/django-contacts
+* https://github.com/iiilx/django-addressbook
---
+++ rubrica/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RubricaConfig(AppConfig):
+    name = 'rubrica'
+    verbose_name = "Rubrica"
